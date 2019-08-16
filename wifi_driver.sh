9f3e9dacc06fc0a07b69a6c44f1f67b4874117c3
Harsh Mangal <51486214+harshmangal1108@users.noreply.github.com>
2019-08-16 12:39:49 +0530

    install WIFI Driver in LINUX
    in your terminal run : bash wifi_driver.sh

---
+++ wifi_driver.sh
@@ -0,0 +1,23 @@
+
+##Step: 1  First of all enter in Terminal by pressing Ctrl+Alt+T.
+#Please install make command
+sudo apt-get install --reinstall make
+#OR
+#Run this command to install make and all the packages needed to build your code.
+sudo apt-get install build-essential
+#Please update your kernel to lates one
+sudo apt-get dist-upgrade
+#In terminal enter 
+git clone https://github.com/lwfinger/rtlwifi_new.git    ###be Patient
+cd rtlwifi_new
+git checkout origin/extended -b extended
+sudo make install   ##be patient
+sudo modprobe -r rtl8723de
+sudo modprobe rtl8723de
+###if you got error like: modprobe: ERROR: could not insert ‘rtl8723de’: Required key not available
+##You MUST turn off secure booting in your BIOS!
+#Restart your laptop
+#If your laptop is hp i5 then you have to  continue press ESC  then        
+#you will see option for boot so choose bios setting from it  and turn off(disable using F6) secure booting in your BIOS! In boot options
+###if signal is week use
+###sudo modprobe -r rtl8723de && sleep 5 && sudo modprobe rtl8723de ant_sel=2
