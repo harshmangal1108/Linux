8ca9abd6551dadfe644c396f6716cdb54f932749
Harsh Mangal <harsh.mangal1108@gmail.com>
2021-02-09 23:37:06 +0530

    Shell Scripting Learning

---
+++ Bash/localsystem/User.sh
@@ -0,0 +1,40 @@
+#!/bin/bash 
+# shebang
+
+# This script displays details on screen 
+# Display text on screen
+
+echo "Hello from Bash!"
+
+## assing a value to vraibale
+
+word="Harsh"
+
+## Display
+
+echo "$word"
+echo "My name is $word"
+
+##append text 
+echo "${word}it!"
+
+## how not to append text 
+echo "$wordit, !"
+
+ending='Mangal'
+
+echo "$word $ending"
+
+## Reassign
+
+word="slash"
+
+echo "${word}harsh is username at all for Harsh $ending"
+
+
+
+
+
+
+
+
